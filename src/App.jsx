0a1686494de52db64ed516d056e321a4710c71d8
Jhobert <joberterato@gmail.com>
2023-12-06 20:43:33 +0800

    start landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import LandingPage from './pages/LandingPage'
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return (<>
+      <LandingPage />
+    </>)
 }
 
 export default App
