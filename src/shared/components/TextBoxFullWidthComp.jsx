437b28eb7c168144660d63b21fd199127375f560
Jhobert <joberterato@gmail.com>
2023-12-12 21:14:01 +0800

    Login

--- src/shared/components/TextBoxFullWidthComp.jsx
+++ src/shared/components/TextBoxFullWidthComp.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 
 
-const TextBoxFullWidthComp = ({id, label, type}) => {
+const TextBoxFullWidthComp = ({id, label, type, size, margin }) => {
 const style = {
     borderRadius: "8px",
     background: "#E7E8E9",
     boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-    margin: "5px",
+    margin: margin,
     "& fieldset": { border: 'none' }
 }
   return (
     <>
-      <TextField id={id} placeholder={label} variant="outlined" sx={style} type={type} fullWidth />
+      <TextField id={id} placeholder={label} variant="outlined" sx={style} type={type} size={size} fullWidth />
     </>
   )
 }
