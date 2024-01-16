b712666eef113c2f997f186d242eeeb74fac44a3
david <judedavid1231@gmail.com>
2024-01-16 22:42:33 +0800

    update landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Box, Typography, Button } from '@mui/material'
 import bg from '../assets/landingBackground1.png'
 import logo from '../assets/Logo.png'
+import title from '../assets/title.png'
+import './LandingPage.css'
 const LandingPage = () => {
   return (<>
         <Box
@@ -9,10 +11,26 @@ const LandingPage = () => {
                 background: `url(${bg})`,
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: '100%',
-                height: '1806px',
-                margin: 0
+                height: '1080px',
+                margin: 0,
+                padding: '8em 12em'
             }}
-        />
+        >
+            <div style={{display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{display: 'flex'}}>
+                    <img src={logo} alt="logo" style={{width: '55px', height: '55px'}} />
+                    <img src={title} alt="title" style={{margin: 'auto 20px', width: 'auto', height: '13px'}} />
+                </div>
+                
+                <ul style={{listStyleType: 'none', margin: '0', padding: '0', float: 'right'}}>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>DISCOVER</li>
+                    <li>CONTACT US</li>
+                </ul>
+            </div>
+            
+        </Box>
     </>)
 }
 
