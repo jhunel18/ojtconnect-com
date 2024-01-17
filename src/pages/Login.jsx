ce5f606fffd9f28bc243b9585243d799b2d74359
Jhobert <joberterato@gmail.com>
2024-01-17 21:02:46 +0800

    Start Calendar Component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,12 +5,14 @@ import HomePage from './HomePage'
 import ServicesComp from '../components/ServicesComp'
 import DiscoverComp from '../components/DiscoverComp'
 import LandingPage from './LandingPage'
+import CalendarComp from '../components/CalendarComp'
 
 const Login = () => {
   return (<>
         {/* <Header /> */}
         <Routes>
             <Route path='' element={<LandingPage />} />
+            <Route path='/calendar' element={<CalendarComp />} />
             <Route path='/login' element={<HomePage />} />
             <Route path='/signup' element={<HomePage />} />
             <Route path='/discover' element={<DiscoverComp />} />
