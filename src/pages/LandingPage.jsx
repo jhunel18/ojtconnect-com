1aeb7996b7fe90bd80538959c9e0665c2198d0a9
david <judedavid1231@gmail.com>
2024-01-18 22:27:45 +0800

    update landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, Button } from '@mui/material'
+import { Box, Typography, Button, Grid, Container } from '@mui/material'
 import bg from '../assets/landingBackground1.png'
 import logo from '../assets/Logo.png'
 import title from '../assets/title.png'
@@ -7,34 +7,57 @@ import './LandingPage.css'
 const LandingPage = () => {
   return (<>
         <Box
-            sx={{
-                background: `url(${bg})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: '100%',
-                height: '1080px',
-                margin: 0,
-                padding: '8em 12em'
-            }}
+        sx={{
+            background: `url(${bg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            height: '70vh',
+            padding: '7em',
+        }}
         >
-            <div style={{display: 'flex', justifyContent: 'space-between' }}>
-                <div style={{display: 'flex'}}>
-                    <img src={logo} alt="logo" style={{width: '55px', height: '55px'}} />
-                    <img src={title} alt="title" style={{margin: 'auto 20px', width: 'auto', height: '13px'}} />
-                </div>
-                
-                <ul className='inter-font' style={{listStyleType: 'none', margin: '0', padding: '0', float: 'right'}}>
-                    <li>HOME</li>
-                    <li>ABOUT</li>
-                    <li>DISCOVER</li>
-                    <li>CONTACT US</li>
-                </ul>
-            </div>
+            <Container maxWidth="lg">
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item lg={5} md={7} xs={12}>
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <img src={logo} alt="logo" style={{ width: '55px', height: '55px' }} />
+                            <img src={title} alt="title" style={{ margin: 'auto 20px', width: 'auto', height: '13px' }} />
+                        </div>
+                        </div>
+                    </Grid>
+                    <Grid item lg={7} md={7} xs={12}>
+                        <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
+                            <ul className='inter-font' style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+                            <li style={{ margin: '0 10px', fontWeight: '600' }}>HOME</li>
+                            <li style={{ margin: '0 10px', fontWeight: '600' }}>ABOUT</li>
+                            <li style={{ margin: '0 10px', fontWeight: '600' }}>DISCOVER</li>
+                            <li style={{ margin: '0 10px', fontWeight: '600' }}>CONTACT US</li>
+                            </ul>
+                        </div>
+                    </Grid>
+                </Grid>
 
-            <Box style={{width: '650px', height: 'auto', display: 'grid', marginTop: '140px'}}>
-                <h1 className='league-spartan-font' style={{fontSize: '55px', fontWeight: '800', margin: 'auto 30px', color: '#F26822', letterSpacing: '5px'}}>Get Started Your Learning & <br />Enrich Your Future</h1>
-                <p className='inter-font' style={{marginLeft: '30px'}}>Est Quis ipsam aut fuga magnam aut accusantium molestias est quam quidem et deleniti maiores et vitae dolore. Ex explicabo suscipit aut asperiores saepe qui vero odio sed suscipit autem a repellendus praesentium?</p>
-            </Box>
-            
+                <Grid container spacing={2} sx={{ marginTop: '9em' }}>
+                    <Grid item lg={7} md={12}>
+                        <h1 className='league-spartan-font' style={{ fontSize: 'clamp(36px, 10vw, 55px)', fontWeight: '800', margin: 'auto 30px', color: '#F26822', letterSpacing: '5px' }}>
+                            Get Started Your Learning & <br /> Enrich Your Future
+                        </h1>
+                    </Grid>
+                    <Grid item lg={7} md={12}>
+                        <p className='inter-font' style={{ marginLeft: '30px' }}>
+                            Est Quis ipsam aut fuga magnam aut accusantium molestias est quam quidem et deleniti maiores et vitae dolore. Ex explicabo suscipit aut asperiores saepe qui vero odio sed suscipit autem a repellendus praesentium?
+                        </p>
+                    </Grid>
+                </Grid>
+
+                <Grid container spacing={2} sx={{ marginTop: '3em', alignItems: 'center' }}>
+                    <Grid item lg={12} md={12} sx={{ textAlign: 'right' }}>
+                        <button style={{ backgroundColor: '#F26822', color: '#fff', borderRadius: '30px', border: '0', padding: '10px 30px', fontSize: '1.5rem', fontWeight: '600', width: '18%', boxShadow: '-5px 5px 10px #8b8b8b, 5px -5px 10px #ffffff', marginLeft: 'auto' }}>
+                        SIGN UP
+                        </button>
+                    </Grid>
+                </Grid>
+            </Container>
         </Box>
     </>)
 }
