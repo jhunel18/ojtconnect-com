9108e4ee422cec989aed64b58d7543c3f7a9ea10
david <judedavid1231@gmail.com>
2024-01-19 22:25:03 +0800

    update landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,35 +3,36 @@ import { Box, Typography, Button, Grid, Container } from '@mui/material'
 import bg from '../assets/landingBackground1.png'
 import logo from '../assets/Logo.png'
 import title from '../assets/title.png'
+import thirdbg from '../assets/sec3Background.png'
 import './LandingPage.css'
 const LandingPage = () => {
-  return (<>
+    return (<>
         <Box
-        sx={{
-            background: `url(${bg})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            height: '70vh',
-            padding: '7em',
-        }}
+            sx={{
+                background: `url(${bg})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: '70vh',
+                padding: '7em',
+            }}
         >
             <Container maxWidth="lg">
                 <Grid container spacing={2} alignItems="center">
                     <Grid item lg={5} md={7} xs={12}>
                         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                            <img src={logo} alt="logo" style={{ width: '55px', height: '55px' }} />
-                            <img src={title} alt="title" style={{ margin: 'auto 20px', width: 'auto', height: '13px' }} />
-                        </div>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <img src={logo} alt="logo" style={{ width: '55px', height: '55px' }} />
+                                <img src={title} alt="title" style={{ margin: 'auto 20px', width: 'auto', height: '13px' }} />
+                            </div>
                         </div>
                     </Grid>
                     <Grid item lg={7} md={7} xs={12}>
                         <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
                             <ul className='inter-font' style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                            <li style={{ margin: '0 10px', fontWeight: '600' }}>HOME</li>
-                            <li style={{ margin: '0 10px', fontWeight: '600' }}>ABOUT</li>
-                            <li style={{ margin: '0 10px', fontWeight: '600' }}>DISCOVER</li>
-                            <li style={{ margin: '0 10px', fontWeight: '600' }}>CONTACT US</li>
+                                <li><a className='nav-link' href='#'>HOME</a></li>
+                                <li><a className='nav-link' href='#'>ABOUT</a></li>
+                                <li><a className='nav-link' href='#'>DISCOVER</a></li>
+                                <li><a className='nav-link' href='#'>CONTACT US</a></li>
                             </ul>
                         </div>
                     </Grid>
@@ -53,11 +54,46 @@ const LandingPage = () => {
                 <Grid container spacing={2} sx={{ marginTop: '3em', alignItems: 'center' }}>
                     <Grid item lg={12} md={12} sx={{ textAlign: 'right' }}>
                         <button style={{ backgroundColor: '#F26822', color: '#fff', borderRadius: '30px', border: '0', padding: '10px 30px', fontSize: '1.5rem', fontWeight: '600', width: '18%', boxShadow: '-5px 5px 10px #8b8b8b, 5px -5px 10px #ffffff', marginLeft: 'auto' }}>
-                        SIGN UP
+                            SIGN UP
                         </button>
                     </Grid>
                 </Grid>
             </Container>
+        </Box>
+
+        <Box sx={{ height: '50vh', display: 'flex', alignItems: 'center' }}>
+            <Grid container>
+                <Grid item lg={4} md={4} sm={10} sx={{ margin: 'auto', textAlign: 'center' }}>
+                    <h2 className='tagline inter-font'>Ut omnis nobis et culpa recusandae ut temporibus consectetur et sunt repellendus. Aut expedita galisum non cons.</h2>
+                </Grid>
+            </Grid>
+        </Box>
+
+        <Box
+            sx={{
+                height: '90vh',
+                background: `url(${thirdbg})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                display: 'flex',
+                alignItems: 'center' 
+            }}>
+            
+            <Grid container spacing={2}>
+                <Grid item lg={8} md={10} sx={{margin: 'auto'}}>
+                    <div style={{width: '100%', height: '70vh', background: '#FD9F5A', borderRadius: '25px', boxShadow:  '8px 8px 16px #bebebe, -8px -8px 16px #ffffff'}}>
+                        <Grid container>
+                            <Grid item lg={6} md={6}>
+
+                            </Grid>
+                            <Grid item lg={6} md={6}>
+
+                            </Grid>
+                        </Grid>
+                    </div>    
+                </Grid> 
+            </Grid>
+
         </Box>
     </>)
 }
