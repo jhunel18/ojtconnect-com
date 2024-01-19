e513795c32e30558b9137c89e6a17e9dea1bd045
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-01-20 00:36:14 +0800

    Fix Signup

--- src/components/StudentSignupComp.jsx
+++ src/components/StudentSignupComp.jsx
@@ -16,44 +16,32 @@ const StudentSignupComp = ({setShowLogin}) => {
   }
   return (
    <>
-     <Typography variant='h5' fontWeight={'bold'} mt={"25px"}>
+     <Typography variant='h5' fontWeight={'bold'} mt={"20px"}>
       Welcome to <span style={{color: '#F26822'}}>OJT Connect</span>
     </Typography>
     <Typography variant='body2' fontWeight={'bolder'} mt={0} mb={2}>
       Let's Get Started! <span style={{textDecoration: 'underline', cursor: 'pointer'}} onClick={handleSetShowLogin}>Login here.</span>
     </Typography>
-    <Box>
+    <Box sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
         <form onSubmit={() => console.log("test")}>
           <TextBoxFullWidthComp id={"firstName"} label={"First Name"} size={'small'} margin={"10px 0px"} />
           <TextBoxFullWidthComp id={"lastName"} label={"Last Name"} size={'small'} margin={"10px 0px"} />
           <TextBoxFullWidthComp type={"email"} id={"email"} label={"Email Address"} size={'small'} margin={"10px 0px"} />
           <TextBoxFullWidthComp type={"password"} id={"password"} label={"Password"} size={'small'} margin={"10px 0px"} />
           <TextBoxFullWidthComp  id={"confirmPassword"} label={"Confirm Password"} size={'small'} margin={"10px 0px"} />
-          <Box display={'flex'} justifyContent={'space-around'} alignItems={'center'}>
-            <Box sx={{flex: 1, mr: 3}}>
-              <TextBoxFullWidthComp type={"password"} id={"code"} label={"Verification Code"} size={'small'} margin={"10px 0px"}/>
-            </Box>
-            <ButtonContainedComp label={"Send To Email"} size={'small'}/>
-          </Box>
+          <TextBoxFullWidthComp  id={"id_number"} label={"ID Number"} size={'small'} margin={"10px 0px"} />
+          <TextBoxFullWidthComp  id={"upload_id"} label={"Upload ID"} size={'small'} margin={"10px 0px"} />
+        
         </form>
+        <Box display={'flex'} justifyContent={'center'} m={'5px 0 10px 0'} alignItems={'center'}>
+          <CheckboxComp />
+          <Typography variant='body2'>
+            I have read the terms and condition.
+          </Typography>
+       </Box>
+        <ButtonContainedComp label={"Submit"} size={'small'} padding={'4px 70px'}/>
     </Box>
-    <Box display={'flex'} justifyContent={'center'} m={'5px 0 10px 0'} alignItems={'center'}>
-        <CheckboxComp />
-        <Typography variant='body2'>
-          I have read the terms and condition.
-        </Typography>
-    </Box>
-
-    <hr />
-      
-    <Typography variant='body2' mt={0} textAlign={'center'}>
-      Or login with
-    </Typography>
-    <Box display={'flex'} alignItems={'center'} justifyContent={'space-evenly'} mt={'20px'}>
-      <ButtonIconComp logo={<FacebookOutlinedIcon />} size={"large"} color={'primary'}/>
-      <ButtonIconComp logo={<GoogleIcon />} size={"large"} color={'primary'}/>
-      <ButtonIconComp logo={<LinkedInIcon />} size={"large"} color={'primary'}/>
-    </Box>
+   
    </>
   )
 }
