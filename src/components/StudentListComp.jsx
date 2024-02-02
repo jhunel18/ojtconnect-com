890bde3411a15d89b260c1a13b7e4341e77d1b11
Jhobert <joberterato@gmail.com>
2024-02-02 21:18:16 +0800

    Student List

--- src/components/StudentListComp.jsx
+++ src/components/StudentListComp.jsx
@@ -1,6 +1,6 @@
-import { Box, Paper, Typography } from '@mui/material'
+import { Box, Paper, Typography, Divider, IconButton } from '@mui/material'
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import StarOutlineRoundedIcon from '@mui/icons-material/StarOutlineRounded';
 import React from 'react'
 
 const StudentListComp = ({student = {
@@ -15,15 +15,21 @@ const StudentListComp = ({student = {
             sx={{
                 background: '#FDFBFA',
                 p: '50px 30px',
-                borderRadius: '30px',
+                borderRadius: '20px',
                 m: '30px',
                 '&:hover':{
-                    borderLeft: '10px solid #FFDDC3'
+                    borderLeft: '30px solid #FFDDC3'
                 }
             }}
             elevation={7}
         >
-            <Box display={'flex'} justifyContent={'flex-start'} alignItems={'center'} mb={'10px'}>
+            <Box 
+                display={'flex'}
+                justifyContent={'flex-start'}
+                alignItems={'center'}
+                mb={'10px'}
+                p={"0 20px"}
+            >
                 <Typography variant='h5'
                     sx={{
                         fontWeight: 'bolder',
@@ -32,10 +38,50 @@ const StudentListComp = ({student = {
                 >
                     {student.name}
                 </Typography>
-                <EmailOutlinedIcon sx={{fontSize: 37, color: '#F26822', mr: '10px'}}/>
-                <StarBorderOutlinedIcon sx={{fontSize: 40, color: '#F26822'}}/>
+                <Box>
+                </Box>
+                <IconButton>
+                    <EmailOutlinedIcon sx={{fontSize: 37, color: '#F26822',}}/>
+                </IconButton>
+                <IconButton>
+                    <StarOutlineRoundedIcon sx={{fontSize: 40, color: '#F26822'}}/>
+                </IconButton>
             </Box>
-            <hr />
+            <Divider variant='fullWidth'/>
+            <Box p={"20px 80px"}>
+                <Box display={'flex'} justifyContent={'space-between'} alignItems={'flex-start'}>
+                    <Box flex={1}>
+                        <Typography fontWeight={'bold'}>Lorem:</Typography>
+                    </Box>
+                    <Box flex={2}>
+                        <Typography>Lorem ipsum A. Amet</Typography>
+                    </Box>
+                </Box>
+                <Box display={'flex'} justifyContent={'space-between'} alignItems={'flex-start'}>
+                    <Box flex={1}>
+                        <Typography fontWeight={'bold'}>Ipsum:</Typography>
+                    </Box>
+                    <Box flex={2}>
+                        <Typography>Pariatur State University</Typography>
+                    </Box>
+                </Box>
+                <Box display={'flex'} justifyContent={'space-between'} alignItems={'flex-start'}>
+                    <Box flex={1}>
+                        <Typography fontWeight={'bold'}>Adolor:</Typography>
+                    </Box>
+                    <Box flex={2}>
+                        <Typography>Computer Engineering</Typography>
+                    </Box>
+                </Box>
+                <Box display={'flex'} justifyContent={'space-between'} alignItems={'flex-start'}>
+                    <Box flex={1}>
+                        <Typography fontWeight={'bold'}>Sit Amet:</Typography>
+                    </Box>
+                    <Box flex={2}>
+                        <Typography>Mollitia et repellat facilis et iure dolorem.</Typography>
+                    </Box>
+                </Box>
+            </Box>
         </Paper>    
     </>)
 }
