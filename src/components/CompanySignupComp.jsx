2192655cd2fddf42cafaf191b402bb1929bda4b3
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-02-02 17:07:17 +0800

    reusable hooks useToggle

--- src/components/CompanySignupComp.jsx
+++ src/components/CompanySignupComp.jsx
@@ -4,6 +4,8 @@ import TextBoxFullWidthComp  from '../shared/components/TextBoxFullWidthComp'
 import CheckboxComp          from '../shared/components/CheckboxComp';
 import ButtonContainedComp   from '../shared/components/ButtonContainedComp';
 import { useNavigate }       from 'react-router-dom';
+import useToggle             from '../hooks/useToggle';
+import TermAndCondition from './TermsAndCondition';
 
 const CompanySignupComp = ({setShowLogin}) => {
 
@@ -13,7 +15,11 @@ const CompanySignupComp = ({setShowLogin}) => {
       setShowLogin(true)
       navigate("/login")
     }
-  
+
+    const {state:Modal, 
+      setState:setModal,
+      toggleState} = useToggle({initialValue:false})
+
   return (
     <>
       <Typography variant='h5' fontWeight={'bold'} mt={"25px"}>
@@ -31,19 +37,29 @@ const CompanySignupComp = ({setShowLogin}) => {
           <TextBoxFullWidthComp type={"password"} id={"password"} label={"Password"} size={'small'} margin={"10px 0px"} />
           <TextBoxFullWidthComp id={"confirmPassword"} label={"Confirm Password"} size={'small'} margin={"10px 0px"} />
         </form>
-        <Box display={'flex'} justifyContent={'center'} m={'5px 0 10px 0'} alignItems={'center'}>
+        <Box display={'flex'} justifyContent={'center'} m={'5px 0 10px 0'} alignItems={'center'} gap={1}>
             <CheckboxComp />
             <Typography variant='body2'
-             sx={{
-               fontSize: 'inherit', 
-              '@media (max-width: 464px)': {
-               fontSize: '12px',},
-               }}>
+                fontWeight={'bolder'}
+                onClick={toggleState}
+                sx={{textDecoration: 'underline',
+                    color: '#F26822', 
+                    fontSize: '14px',
+                    '@media (max-width: 464px)': {
+                    fontSize: '12px',},
+                    '&:hover': {
+                      color: '#F26822',  // Original color
+                      cursor: 'pointer',
+                      filter: 'brightness(0.9)',        
+                    },
+                  }}
+            >
               I have read the terms and condition.
             </Typography>
         </Box>
         <ButtonContainedComp label={"submit"} size={'small'} padding={'4px 70px'}/>
       </Box>
+      {Modal && <TermAndCondition modal={Modal} setModal={setModal}/>}
     </>
   )
 }
