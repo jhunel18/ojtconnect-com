9a7e021b715bdeb3a3934c73af2074d795593bae
Jhobert <joberterato@gmail.com>
2024-02-06 20:26:04 +0800

    Student List Final

--- src/pages/NewsFeedPage/index.jsx
+++ src/pages/NewsFeedPage/index.jsx
@@ -1,40 +1,72 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Container, Hidden, Grid } from '@mui/material'
 import MainLayout from '../../layouts/MainLayout'
 import CalendarComp from '../../components/CalendarComp'
 import SchedulesComp from '../../components/SchedulesComp'
 import StudentListComp from '../../components/StudentListComp'
+import MessagesList from '../../components/MessageList'
+import Notifications from '../../components/Notifications'
+import style from "./style.module.css"
 
 const index = () => {
     return (<>
         <MainLayout>
-            <Box
-                display={'flex'}
-                justifyContent={'center'}
-                alignItems={'center'}
-                width={'100vh'}
-            >
-                <Box sx={{flex: 1}}>
-                    <CalendarComp />
-                    <SchedulesComp />
-                </Box>
-                <Box
-                    sx={{
-                        flex: 2,
-                        background: '#fffff',
-                        p: '2% 1%',
-                        overflow: 'auto'
-                    }}
-                >
-                    <StudentListComp />
-                    <StudentListComp />
-                    <StudentListComp />
-                    <StudentListComp />
-                    <StudentListComp />
-                    <StudentListComp />
-                </Box>
-                <Box sx={{flex: 1}}>Test</Box>
-            </Box>
+            <Grid container spacing={2} sx={{
+                display: 'flex',
+                justifyContent: 'center',
+            }}>
+                <Grid item xs={2} lg={2} xl={2} m={2}>
+                    <div style={{
+                        height: "90vh",
+                        overFlowY: "scroll"
+                    }}>
+                        <CalendarComp />
+                        <br />
+                        <SchedulesComp />
+                    </div>
+                </Grid>
+                <Grid item xs={4} lg={4} xl={4} md={4} m={2}>
+                    <div className={style.student_list} style={{
+                        height: "90vh",
+                        overflow: "auto",
+                    }}>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                        <StudentListComp />
+                        <br/>
+                    </div>
+                </Grid>
+                <Grid item xs={2} lg={2} xl={2} m={2}>
+                    <div style={{
+                        height: "90vh",
+                        overFlowY: "scroll"
+                    }}>
+                        <Notifications />
+                        <br />
+                        <MessagesList />
+                    </div>
+                </Grid>
+            </Grid>
         </MainLayout>
     </>)
 }
