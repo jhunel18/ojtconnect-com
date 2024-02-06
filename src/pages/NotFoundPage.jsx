6a9731ed628e9e273f3fcab431d4674d3dcf87e6
david <judedavid1231@gmail.com>
2024-02-06 22:24:26 +0800

    added NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import MainLayout from '../layouts/MainLayout'
+
+import notfound from '../assets/spiltcoffee.png';
+
+import { Box, Grid } from '@mui/material'
+
 const NotFoundPage = () => {
   return (
-    <div>NotFound</div>
+    <MainLayout>
+      <Grid container sx={{
+          height: '100vh',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          margin: '50px 20px'
+        }}>
+        <Grid item lg={10}>
+          <Box sx={{
+            width: '100%',
+            height: '80vh',
+            background: '#fff',
+            borderRadius: '15px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <img src={notfound} alt='not found' style={{
+              objectFit: 'cover',
+              width: '650px',
+              height: '560px'
+            }} />
+          </Box>
+        </Grid>
+      </Grid>
+    </MainLayout>
   )
 }
 
